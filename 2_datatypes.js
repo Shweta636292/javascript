11e65bae68aa483e5a14d5427cd000a5c5777f25
Shweta636292 <shwetagejjihalli@gmail.com>
2025-12-24 19:17:21 +0530

    datatypes

---
+++ 2_datatypes.js
@@ -0,0 +1,22 @@
+console.log(3+3)
+console.log("datatypes")
+
+let alvasAtmoodbidre=true; //boolean 
+let name="shweta" //string
+let age=18 
+
+
+// number=2
+// string=""
+// boolean=true/false
+// undefined=undefined
+// null=standlone value  
+// symbol=unique
+
+//object
+console.log(typeof "shweta")
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof age)
+console.log(typeof boolean)
+
