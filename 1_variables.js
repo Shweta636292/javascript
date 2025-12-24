67f80e773253ba1ccae3d77777abc00cacfe1f22
Shweta636292 <shwetagejjihalli@gmail.com>
2025-12-24 19:15:24 +0530

    variables

---
+++ 1_variables.js
@@ -0,0 +1,6 @@
+const studentId=63627204
+let studentemail="shweta1234@gmail.com"
+var studentcollege="AIET"
+studentcity="mangalore"
+
+console.table([studentId,studentemail,studentcollege,studentcity])
