0d6f4fe66701e9c3a154763d1bc103ed32720bf4
Shweta636292 <shwetagejjihalli@gmail.com>
2025-12-24 19:20:15 +0530

    comparison

---
+++ 3_conversionandoperation.js
@@ -0,0 +1,51 @@
+let age="33abc"
+
+console.log(typeof age)
+console.log(typeof (age))
+
+let Islinkedin=Number(age)
+console.log(typeof Islinkedin)
+console.log(Islinkedin)
+
+ //33=33
+ //33abc=NaN
+//true=1; false=0
+
+let IsloggedIn=""
+let booleanIsloggedIn=Boolean(IsloggedIn)
+console.log(booleanIsloggedIn)
+
+//""=false
+//"shweta"=true
+
+let someNumber=72
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+//operations
+let value=7
+let negativevalue=-value
+console.log(negativevalue)
+
+let str1="shweta"
+let str2="sindhu"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+4+"2")
+console.log((3*5)+2)
+
+console.log(+true)
+console.log(+"")
+
+let studentNumber=588
+studentNumber++
+console.log(studentNumber)
+
+let seatNumber=588
+++seatNumber
+console.log(seatNumber)
