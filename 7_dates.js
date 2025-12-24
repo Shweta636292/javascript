f36a68fe85d2e04ee714a54d0c61c6ce6d2960e9
Shweta636292 <shwetagejjihalli@gmail.com>
2025-12-24 19:31:11 +0530

    dates

--- 7_dates.js
+++
@@ -1,8 +0,0 @@
-let myDate=new date()
-console.log(myDate)
-
-console.log(myDate.toDateString())
-console.log(myDate.toLocaleString())
-console.log(myDate.toISOString())
-console.log("hello world")
-sdgfgdsfg
