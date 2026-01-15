b2d6c8679d0c550987268e3ccc1fff3b80008695
Shweta636292 <shwetagejjihalli@gmail.com>
2026-01-15 15:45:43 +0530

    Numbers

--- 6_numbers.js
+++ 6_numbers.js
@@ -4,7 +4,7 @@ console.log(Math.round(23.6))
 console.log(Math.ceil(2.8))
 console.log(Math.floor(2.8))
 console.log(Math.max(3,4,5,6))
-console.log(Math.min(4,5,6,7))
+console.log(Math.min(4,5,6,7));
 
 console.log(Math.random())
 console.log(Math.random()*10)+1
